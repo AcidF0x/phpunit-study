40e2ac005104431bc15abe7e6a6481f797047d6f
duhuicho <ic_jdh@me.com>
2020-02-23 17:44:38 +0900

    step3

--- test/CalcTest.php
+++ test/CalcTest.php
@@ -6,35 +6,66 @@ use App\Calc;
  */
 class CalcTest extends TestCase
 {
-    protected $calc;
-
-    protected function setUp(): void
+    public function testEqules()
     {
-        parent::setUp();
-        $this->calc = new Calc();
+        $a = 10;
+        $this->assertEquals(10, $a); //성공
+        $this->assertEquals("10", $a); //성공
     }
 
-    public function testPlus()
+    public function testSame()
     {
-        $result = $this->calc->plus(10, 20);
-        $this->assertEquals(30, $result);
+        $a = 10;
+        $this->assertSame(10, $a); //성공
+        // $this->assertSame("10", $a); //실패
     }
 
-    public function testMinus()
+    public function testCount()
     {
-        $result = $this->calc->minus(10, 20);
-        $this->assertEquals(-10, $result);
+        $array = [ 1, 2, 3, 4 ];
+        $this->assertCount(count($array), $array); //성공
+        // $this->assertCount(1, $array); // 실패
     }
 
-    public function testMultiply()
+//    public function testBools()
+//    {
+//        $result = doSomeThing();
+//        $this->assertTrue($result);
+//        $this->assertFalse($result);
+//        $this->assertNull($result);
+//    }
+
+    public function testContains()
     {
-        $result = $this->calc->multiply(10, 20);
-        $this->assertEquals(200, $result);
+        $array = [ 1, 2, 3, 4 ];
+        $this->assertContains(3, $array); // 성공
+        $this->assertNotContains(10, $array); //성공
+        //$this->assertContains(10, $array); //실패
     }
 
-    public function testDivision()
+    public function testAssertArrayHasKey()
     {
-        $result = $this->calc->division(10, 5);
-        $this->assertEquals(2, $result);
+        $array = [
+          'key' => 1,
+          'key2' => 2,
+          'key3' => 3
+        ];
+
+        $this->assertArrayHasKey('key', $array); //성공
+        $this->assertArrayNotHasKey('wow', $array); //성공
+//        $this->assertArrayHasKey('key100', $array); //실패
     }
+
+
+    public function testExceptException()
+    {
+        $func = function () {
+            throw new Exception('에러 메시지');
+        };
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('에러 메시지');
+        $func();
+    }
+
 }
